2cad6e2c22f4ab7ec8581a4da2583c9c9bf04298
Indra Firmansyah <indransyah@gmail.com>
2017-10-23 23:00:14 +0700

    Add product image preview test

--- src/components/product/productImage.js
+++ src/components/product/productImage.js
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types'
 import { Col, Image, Row } from 'react-bootstrap'
 
 class ProductImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainImageIndex: 0
+    };
+  }
+
+  handleClick = (index) => {
+    const { mainImageIndex } = this.state
+    if(mainImageIndex !== index) {
+      this.setState({
+        mainImageIndex: index
+      })
+    }
+  }
+
   render() {
     const { images } = this.props
     if(images.length === 0) {
@@ -10,13 +26,13 @@ class ProductImage extends React.Component {
     }
     return (
       <Col xs={12} md={8}>
-        <Image id="preview" className="center-block" src={images[0].path} alt={images[0].name} responsive />
+        <Image id="preview" className="center-block" src={images[this.state.mainImageIndex].path} alt={images[this.state.mainImageIndex].name} responsive />
         <hr/>
         <Row>
           {images.map((value, index) => {
             return (
-              <Col key={value.id} xs={4} md={3}>
-                <Image className="center-block" src={value.path} alt={value.name} thumbnail />
+              <Col key={value.id} xs={4} md={3} onClick={(e) => this.handleClick(index)} style={{cursor: "pointer"}}>
+                <Image href="" className="center-block" src={value.path} alt={value.name} thumbnail />
               </Col>
             )
           })}
--- src/components/product/productImage.test.js
+++ src/components/product/productImage.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { Thumbnail, Row, Image } from 'react-bootstrap'
+import { Thumbnail, Row, Col, Image } from 'react-bootstrap'
 import ProductImage from './productImage'
 import { formatProductDetail } from '../../helper'
 import { productDetailResponse } from '../../samples/product'
@@ -33,4 +33,17 @@ it('should have thumbnail images', () => {
   expect(wrapper.find(Row).children().length).toBe(product.images.length)
   expect(wrapper.find(Row).find(Image).first().props().src).toBe(product.images[0].path)
   expect(wrapper.find(Row).find(Image).last().props().src).toBe(product.images[product.images.length - 1].path)
+})
+
+it('should change the main image', () => {
+  // Before click
+  let mainImageIndex = wrapper.state().mainImageIndex
+  expect(wrapper.find('#preview').prop('src')).toBe(product.images[mainImageIndex].path)
+
+  wrapper.find(Row).find(Col).last().simulate('click')
+
+  // After click
+  mainImageIndex = wrapper.state().mainImageIndex
+  expect(wrapper.state().mainImageIndex).toEqual(product.images.length - 1)
+  expect(wrapper.find('#preview').prop('src')).toBe(product.images[mainImageIndex].path)
 })
