20069bac6ebbac64dbde2effe55fb65e81777426
Indra Firmansyah <indransyah@gmail.com>
2017-10-23 11:18:13 +0700

    Remove Link component

--- src/pages/productListPage.js
+++ src/pages/productListPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Grid, Row, Col, Thumbnail, Pagination } from 'react-bootstrap'
 
 class ProductListPage extends React.Component {
@@ -10,85 +9,67 @@ class ProductListPage extends React.Component {
         <h5>Products</h5>
         <Row>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
           <Col className="text-center" xs={6} md={4}>
-            <Link to="/product">
-              <Thumbnail src="http://fakeimg.pl/254x350/" alt="254x350">
-                <h5>Product Name</h5>
-                <h5>Price</h5>
-                <h6>Category</h6>
-              </Thumbnail>
-            </Link>
+            <Thumbnail href="/product" src="http://fakeimg.pl/254x350/" alt="254x350">
+              <h5>Product Name</h5>
+              <h5>Price</h5>
+              <h6>Category</h6>
+            </Thumbnail>
           </Col>
         </Row>
         <Pagination
